c57a68c16892c4dc181c597d19af063509ec3566
Rosan D <113587383+infernozen@users.noreply.github.com>
2024-06-02 17:46:11 +0530

    Update secrets.js

--- utils/secrets.js
+++ utils/secrets.js
@@ -1,44 +1,29 @@
-// const {
-//     DB_HOST,
-//     DB_USER,
-//     DB_PASS,
-//     DB_NAME,
-//     JWT_SECRET_KEY
-// } = process.env;
-
 const {
-    DB_HOST,
-    DB_USER,
-    DB_PASS,
-    DB_NAME,
-    JWT_SECRET_KEY,
-    OTP_EMAIL_ADDRESS,
-    OTP_EMAIL_PASSWORD,
-} = {
-    DB_HOST: '34.31.27.226',
-    DB_USER: 'root',
-    DB_PASS: 'root',
-    DB_NAME: 'userinfo',
-    JWT_SECRET_KEY: 'faszen@ssn#RoCube$tech',
-    OTP_EMAIL_ADDRESS: 'spam2210561@gmail.com',
-    OTP_EMAIL_PASSWORD: 'oaso wymh khjf nmru',
-};
-
+   DB_HOST,
+   DB_USER,
+   DB_PASS,
+   DB_NAME,
+   JWT_SECRET_KEY,
+   OTP_EMAIL_ADDRESS,
+   OTP_EMAIL_PASSWORD,
+} = process.env;
 
 const requiredCredentials = [
     'DB_HOST',
     'DB_USER',
     'DB_PASS',
     'DB_NAME',
-    'JWT_SECRET_KEY'
+    'JWT_SECRET_KEY',
+    'OTP_EMAIL_ADDRESS',
+    'OTP_EMAIL_PASSWORD',
 ];
 
-// for (const credential of requiredCredentials) {
-//     if (process.env[credential] === undefined) {
-//         console.log(`Missing required crendential: ${credential}`);
-//         process.exit(1);
-//     }
-// }
+for (const credential of requiredCredentials) {
+   if (process.env[credential] === undefined) {
+     console.log(`Missing required crendential: ${credential}`);
+     process.exit(1);
+   }
+}
 
 module.exports = {
     DB_HOST,
